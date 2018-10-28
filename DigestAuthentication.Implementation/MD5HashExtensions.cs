2e482e204f36d172857b1b7f1d837ebeb4c202be
Eddie Stanley <eddie@eddiewould.com>
2018-10-28 22:36:00 +1000

    Hacking away on support for net45.
    - Half-way through updating DigestAuthentication.AspNetClassic.csproj to the 2017 format
    - Have added a pure C# implementation of MD5 (gist from github...)
    - Have changed NuGet references to project references (will want to revert that)

--- DigestAuthentication.Implementation/MD5HashExtensions.cs
+++ DigestAuthentication.Implementation/MD5HashExtensions.cs
@@ -1,18 +1,25 @@
-﻿using System;
+﻿#if NETSTANDARD1_3
 using System.Security.Cryptography;
+#endif
 using System.Text;
 
 namespace FlakeyBit.DigestAuthentication.Implementation
 {
     internal static class MD5HashExtensions
     {
+#if NETSTANDARD1_3
         public static string ToMD5Hash(this byte[] bytes) {
-            var hashBytes = MD5.Create().ComputeHash(bytes);
-            return BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant();
+            var hashBytes = System.Security.Cryptography.MD5.Create().ComputeHash(bytes);
+            return System.BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant();
         }
+#endif
 
         public static string ToMD5Hash(this string inputString) {
+#if NETSTANDARD1_3
             return Encoding.UTF8.GetBytes(inputString).ToMD5Hash();
+#else
+            return MD5.Calculate(Encoding.UTF8.GetBytes(inputString));
+#endif
         }
     }
 }
