8b369379d40ad774485f305ea5bfe3f9b1d7ee93
Eddie Stanley <eddie@eddiewould.com>
2019-03-20 22:55:07 +1000

    Various tidying of https://github.com/flakey-bit/DotNetDigestAuth/pull/1

--- DigestAuthentication.Implementation/DigestAuthImplementation.cs
+++ DigestAuthentication.Implementation/DigestAuthImplementation.cs
@@ -43,45 +43,39 @@ namespace FlakeyBit.DigestAuthentication.Implementation
                 ("algorithm", "MD5", false)
             };
 
-            return "Digest " + String.Join(", ", parts.Select(FormatChallengeHeaderComponent));
+            return "Digest " + String.Join(", ", parts.Select(FormatHeaderComponent));
         }
 
-		public async Task<string> BuildAuthInfoHeader(DigestChallengeResponse response) {
-
+		public async Task<string> BuildAuthInfoHeaderAsync(DigestChallengeResponse response) {
 			var timestampStr = response.Nonce.Substring(0, NonceTimestampFormat.Length);
-			var timestamp = DateTimeOffset.ParseExact(timestampStr, NonceTimestampFormat, CultureInfo.InvariantCulture);
+			var timestamp = ParseTimestamp(timestampStr);
 
 			var delta = timestamp - DateTime.UtcNow;
 			var deltaSeconds = Math.Abs(delta.TotalSeconds);
 
-			List<ValueTuple<string, string, bool>> parts = new List<ValueTuple<string, string, bool>>()
-			{
+			List<ValueTuple<string, string, bool>> parts = new List<ValueTuple<string, string, bool>> {
 				("qop", QopMode, true),
 				("rspauth", await CreateRspAuth(response), true),
 				("cnonce", response.ClientNonce, true),
 				("nc", response.NonceCounter, false)
 			};
 
-			if (Math.Abs(deltaSeconds - _config.MaxNonceAgeSeconds) < _config.DeltaSecondsToNextNonce)
-			{
+			if (Math.Abs(deltaSeconds - _config.MaxNonceAgeSeconds) < _config.DeltaSecondsToNextNonce) {
 				parts = parts.Prepend(("nextnonce", CreateNonce(DateTime.UtcNow), true)).ToList();
 			}
 
-			return String.Join(", ", parts.Select(FormatChallengeHeaderComponent));
+			return String.Join(", ", parts.Select(FormatHeaderComponent));
 		}
 
-		private string FormatChallengeHeaderComponent((string Key, string Value, bool ShouldQuote) component) {
+        private string FormatHeaderComponent((string Key, string Value, bool ShouldQuote) component) {
             if (component.ShouldQuote) {
                 return $"{component.Key}=\"{component.Value}\"";
             }
 
             return $"{component.Key}={component.Value}";
         }
-
-
 		
         public async Task<string> ValidateChallangeAsync(DigestChallengeResponse challengeResponse, string requestMethod) {
-
             if (challengeResponse == null) {
 	            return null;
             }
@@ -115,6 +109,18 @@ namespace FlakeyBit.DigestAuthentication.Implementation
             return $"{a1Hash}:{response.Nonce}:{response.NonceCounter}:{response.ClientNonce}:{QopMode}:{a2Hash}".ToMD5Hash();
         }
 
+        private async Task<string> CreateRspAuth(DigestChallengeResponse response)
+        {
+            var secretForUsername = await _usernameSecretProvider.GetSecretForUsernameAsync(response.Username);
+            var a1 = $"{response.Username}:{response.Realm}:{secretForUsername}";
+            var a1Hash = a1.ToMD5Hash();
+
+            var a2 = $":{response.Uri}";
+            var a2Hash = a2.ToMD5Hash();
+
+            return $"{a1Hash}:{response.Nonce}:{response.NonceCounter}:{response.ClientNonce}:{QopMode}:{a2Hash}".ToMD5Hash();
+        }
+
         private string CreateNonce(DateTime timestamp) {
             var builder = new StringBuilder();
             var timestampStr = timestamp.ToString(NonceTimestampFormat);
@@ -128,12 +134,11 @@ namespace FlakeyBit.DigestAuthentication.Implementation
         private bool ValidateNonce(DigestChallengeResponse challengeResponse) {
             try {
                 var timestampStr = challengeResponse.Nonce.Substring(0, NonceTimestampFormat.Length);
-                var timestamp = DateTimeOffset.ParseExact(timestampStr, NonceTimestampFormat, CultureInfo.InvariantCulture);
+                var timestamp = ParseTimestamp(timestampStr);
 
                 var delta = timestamp - DateTime.UtcNow;
 
 				if (Math.Abs(delta.TotalSeconds) > _config.MaxNonceAgeSeconds) {
-	                
                     return false;
                 }
 
@@ -143,16 +148,9 @@ namespace FlakeyBit.DigestAuthentication.Implementation
             }
         }
 
-        private async Task<string> CreateRspAuth(DigestChallengeResponse response)
+        private static DateTimeOffset ParseTimestamp(string timestampStr)
         {
-	        var secretForUsername = await _usernameSecretProvider.GetSecretForUsernameAsync(response.Username);
-			var a1 = $"{response.Username}:{response.Realm}:{secretForUsername}";
-	        var a1Hash = a1.ToMD5Hash();
-
-	        var a2 = $":{response.Uri}";
-	        var a2Hash = a2.ToMD5Hash();
-
-	        return $"{a1Hash}:{response.Nonce}:{response.NonceCounter}:{response.ClientNonce}:{QopMode}:{a2Hash}".ToMD5Hash();
+            return DateTimeOffset.ParseExact(timestampStr, NonceTimestampFormat, CultureInfo.InvariantCulture);
         }
     }
 }
