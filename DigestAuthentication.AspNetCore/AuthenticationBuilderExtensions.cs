28bf5eb2130e6aa56aa3186cee264709b53691ad
Eddie Stanley <eddie@eddiewould.com>
2021-08-14 13:31:07 +1200

    [GH-12]: Support custom scheme name / display name

--- DigestAuthentication.AspNetCore/AuthenticationBuilderExtensions.cs
+++ DigestAuthentication.AspNetCore/AuthenticationBuilderExtensions.cs
@@ -5,13 +5,30 @@ namespace FlakeyBit.DigestAuthentication.AspNetCore
 {
     public static class AuthenticationBuilderExtensions
     {
+        /// <summary>
+        /// Enables digest authentication
+        /// </summary>
+        /// <param name="builder">The <see cref="AuthenticationBuilder"/></param>
+        /// <param name="config">Mandatory configuration for Digest Authentication</param>
+        /// <returns>A reference to builder after the operation has completed.</returns>
+        public static AuthenticationBuilder AddDigestAuthentication(this AuthenticationBuilder builder, DigestAuthenticationConfiguration config) =>
+            builder.AddDigestAuthentication(config, "Digest", "Digest");
+
+        /// <summary>
+        /// Enables digest authentication
+        /// </summary>
+        /// <param name="builder">The <see cref="AuthenticationBuilder"/></param>
+        /// <param name="config">Mandatory configuration for Digest Authentication</param>
+        /// <param name="authenticationScheme">The authentication scheme.</param>
+        /// <param name="displayName">The display name for the authentication handler.</param>
+        /// <returns>A reference to builder after the operation has completed.</returns>        
         public static AuthenticationBuilder AddDigestAuthentication(this AuthenticationBuilder builder,
-                                                                    DigestAuthenticationConfiguration config) {
-            return builder.AddScheme<DigestAuthenticationOptions, DigestAuthenticationHandler>("Digest",
-                                                                                               "Digest",
-                                                                                               options => {
-                                                                                                   options.Configuration = config;
-                                                                                               });
+                                                                    DigestAuthenticationConfiguration config,
+                                                                    string authenticationScheme,
+                                                                    string displayName) {
+            return builder.AddScheme<DigestAuthenticationOptions, DigestAuthenticationHandler>(authenticationScheme,
+                                                                                               displayName,
+                                                                                               options => { options.Configuration = config; });
         }
     }
 }
